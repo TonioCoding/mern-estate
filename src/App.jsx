69891f74879e66b61835ca77231d700bb33e70a3
TonioCoding <ToneBone7960@gmail.com>
2023-10-07 17:22:15 -0400

    updated changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SignUp from './pages/SignUp'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Header from './components/Header'
 
 const App = () => {
-  return (
-    <div className=''>App</div>
+  return ( 
+  <BrowserRouter>
+  <Header />
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/sign-in" element={<SignIn/>} />
+    <Route path="/sign-up" element={<SignUp />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </BrowserRouter>
   )
 }
 
