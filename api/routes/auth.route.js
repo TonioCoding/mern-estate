69e647b6d994415b6ed0007c0b0e33f1412fae14
TonioCoding <ToneBone7960@gmail.com>
2023-10-14 16:11:06 -0400

    new default profile pic and compacted imports

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { google, signup } from '../controllers/auth.controller.js';
-import { signIn } from '../controllers/auth.controller.js';
+import { google, signup, signIn } from '../controllers/auth.controller.js';
+
 
 const router = express.Router();
 
