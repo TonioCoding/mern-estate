ac1002efeaa0fce22a05e6f8bb331f77af69ce76
TonioCoding <ToneBone7960@gmail.com>
2023-10-28 18:50:20 -0400

    current functionality for search(9:50:52)

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -278,7 +278,7 @@ export default function CreateListing() {
             )) 
           }
           <button disabled={loading || uploading} className='p-3 bg-slate-700 text-white rounded-lg
-         uppercase hover:opacity-95 disabled:opacity-90'>{loading ? 'Creating...' : 'Update Listing'}
+         uppercase hover:opacity-95 disabled:opacity-90'>{loading ? 'Updating...' : 'Update Listing'}
           </button>
         {error && <p className="text-red-700 text-sm">{error}</p>}
         </div>
