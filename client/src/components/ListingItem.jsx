db4ad8fd26644df5351258891b9f3a6e71d5e32e
TonioCoding <ToneBone7960@gmail.com>
2023-10-30 13:27:17 -0400

    created listingitem component and show listings

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -6,7 +6,7 @@ export default function ListingItem({listing}) {
     <div className='bg-white shadow-md hover:shadow-lg
     transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]'>
         <Link to={`/listing/${listing._id}`}>
-            <img src={listing.imageUrls[0]} 
+            <img src={listing.imageUrls[0] || "https://assets.everspringpartners.com/fe/06/f23661be455e97d009c6ae418995/real-estate-finance.jpg"} 
             alt="listing cover" 
             className='h-[320px] sm:h-[220px] w-full object-cover
             hover:scale-105 transition-scale duration-300'
@@ -16,9 +16,27 @@ export default function ListingItem({listing}) {
                 truncate'>{listing.name}</p>
                 <div className="flex items-center gap-1">
                     <MdLocationOn className='h-4 w-4 text-green-700'/>
-                    <p className='text-sm truncate text-gray-600'>{listing.address}</p>
+                    <p className='text-sm truncate text-gray-600 w-full'>{listing.address}</p>
                 </div>
-                
+                <p className='text-sm text-gray-600 line-clamp-2'>
+                  {listing.description}</p>
+                  <p className='text-slate-500 mt-2 font-semibold'>
+                    $
+                    {listing.offer ? listing.discountPrice.
+                    toLocaleString('en-US') : listing.regularPrice.
+                    toLocaleString('en-US')}
+                    {listing.type === 'rent' && ' / month'}
+                  </p>
+                  <div className="text-slate-700 flex gap-4">
+                    <div className="font-bold text-xs">
+                      {listing.bedrooms > 1 ? `${listing.bedrooms}
+                      Beds ` : `${listing.bedrooms} Bed `}
+                    </div>
+                    <div className="font-bold text-xs">
+                      {listing.bathrooms > 1 ? `${listing.bathrooms}
+                      Baths ` : `${listing.bedrooms} Bath `}
+                    </div>
+                  </div>
             </div>
         </Link>
     </div>
