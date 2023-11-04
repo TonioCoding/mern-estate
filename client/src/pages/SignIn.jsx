4bb06fc6ca296cba7c21317ff97bab82304539cc
TonioCoding <ToneBone7960@gmail.com>
2023-11-04 18:41:39 -0400

    cleaner code and documentation

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,17 +1,19 @@
+//imports
 import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice'
 import OAuth from '../components/OAuth'
 
-
+//main function
 export default function SignIn (){
+  //states and intialization
   const [formData, setFormData] = useState({})
   const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-
+  //functions for signin page
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -44,17 +46,31 @@ export default function SignIn (){
     }
   };
 
-  
+  //display
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold
       my-7'>Sign In</h1>
-      <form  onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input type="email" placeholder='email ' className='border
-        p-3 rounded-lg' id='email'onChange={handleChange} />
-        <input type="password" placeholder='password' className='border
-        p-3 rounded-lg' id='password'onChange={handleChange} />
-        <button  disabled={loading} className='bg-slate-700 text-white p-3
+      <form  
+      onSubmit={handleSubmit} 
+      className='flex flex-col gap-4'>
+        <input 
+        type="email" 
+        placeholder='email' 
+        className='border p-3 rounded-lg' 
+        id='email'
+        onChange={handleChange}/>
+
+        <input 
+        type="password" 
+        placeholder='password' 
+        className='border p-3 rounded-lg' 
+        id='password'
+        onChange={handleChange}/>
+
+        <button  
+        disabled={loading} 
+        className='bg-slate-700 text-white p-3
         rounded-lg uppercase hover:opactiy-95 
         disabled:opacity-80'>{loading ? 'Loading...' : 'Sign In'}
         </button>
