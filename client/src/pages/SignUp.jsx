833965a0de01e5dcdb2a8d9b7c2271349754910e
TonioCoding <ToneBone7960@gmail.com>
2023-11-04 18:44:15 -0400

    cleaner code and documentation

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,14 +1,18 @@
+//imports
 import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import OAuth from '../components/OAuth'
 
+
+//main function
 export default function SignUp (){
+  //states and intialization
   const [formData, setFormData] = useState({})
   const [error, setError] = useState(null)
   const [loading, setLoading] = useState(false)
   const navigate = useNavigate();
 
-
+  //functions for signup page
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -16,7 +20,6 @@ export default function SignUp (){
       });
     };
 
-  
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -44,19 +47,39 @@ export default function SignUp (){
     }
   };
 
-  
+  //display
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold
       my-7'>Sign Up</h1>
-      <form  onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input type="text" placeholder='username ' className='border
-        p-3 rounded-lg' id='username'onChange={handleChange} />
-        <input type="email" placeholder='email ' className='border
-        p-3 rounded-lg' id='email'onChange={handleChange} />
-        <input type="password" placeholder='password' className='border
-        p-3 rounded-lg' id='password'onChange={handleChange} />
-        <button  disabled={loading} className='bg-slate-700 text-white p-3
+      <form  
+      onSubmit={handleSubmit} 
+      className='flex flex-col gap-4'>
+
+        <input 
+        type="text" 
+        placeholder='username ' 
+        className='border p-3 rounded-lg' 
+        id='username'
+        onChange={handleChange} />
+
+        <input 
+        type="email"
+        placeholder='email ' 
+        className='border p-3 rounded-lg' 
+        id='email'
+        onChange={handleChange} />
+
+        <input 
+        type="password" 
+        placeholder='password' 
+        className='border p-3 rounded-lg' 
+        id='password'
+        onChange={handleChange} />
+
+        <button  
+        disabled={loading} 
+        className='bg-slate-700 text-white p-3
         rounded-lg uppercase hover:opactiy-95 
         disabled:opacity-80'>{loading ? 'Loading...' : 'Sign Up'}
         </button>
