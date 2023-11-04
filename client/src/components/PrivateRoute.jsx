e7031f956222f3b9c03d7af4563d8daef1b915eb
TonioCoding <ToneBone7960@gmail.com>
2023-11-04 18:58:03 -0400

    documentation

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,6 +1,8 @@
+//imports
 import { useSelector } from "react-redux"
 import { Outlet, Navigate } from "react-router-dom"
 
+//main function
 export const PrivateRoute = () => {
     const {currentUser} = useSelector((state) => state.user)
   return currentUser ? <Outlet /> : <Navigate to='/sign-in' />
